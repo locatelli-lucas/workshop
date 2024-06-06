0185ef9071e920e3dd26eea913935b20b93f6057
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-06-06 16:18:06 -0300

    Service layer, component registration

---
+++ src/main/java/com/locatellilucas/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.locatellilucas.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.locatellilucas.course.entities.User;
+import com.locatellilucas.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
