22aba0e639272f38a9d541afce6ea54e730c498e
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-06-19 14:01:20 -0300

    User Delete

--- src/main/java/com/locatellilucas/course/resources/UserResource.java
+++ src/main/java/com/locatellilucas/course/resources/UserResource.java
@@ -1,13 +1,11 @@
 package com.locatellilucas.course.resources;
 
-import java.io.Serializable;
 import java.net.URI;
 import java.util.List;
 
-import jakarta.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.locatellilucas.course.entities.User;
 import com.locatellilucas.course.services.UserService;
@@ -44,6 +41,12 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();	
 	}
 	
 }
