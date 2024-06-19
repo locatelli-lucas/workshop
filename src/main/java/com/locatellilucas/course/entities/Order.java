ba7cac17c5f7886f3d3fb100442003aa51fbcdf2
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-06-19 11:22:04 -0300

    Payment, one-to-one association

--- src/main/java/com/locatellilucas/course/entities/Order.java
+++ src/main/java/com/locatellilucas/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -39,6 +41,9 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 	
 	public Order() {
 		
@@ -86,6 +91,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 	public Set<OrderItem> getItems() {
 		return items;
