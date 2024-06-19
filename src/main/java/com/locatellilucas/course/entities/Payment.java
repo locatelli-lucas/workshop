aec54bd59099605b0cd0791ac2ec2b61bb3148c2
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-06-19 13:32:57 -0300

    Subtotal & Total methods

--- src/main/java/com/locatellilucas/course/entities/Payment.java
+++ src/main/java/com/locatellilucas/course/entities/Payment.java
@@ -12,6 +12,8 @@ import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
@@ -22,6 +24,7 @@ public class Payment implements Serializable {
 	private Long id;
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId	
 	private Order order;
